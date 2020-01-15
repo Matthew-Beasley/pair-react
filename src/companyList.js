57d9245125afd8d6e5cd09c1f7ef2fd60772decd
conbec <56614504+conbec@users.noreply.github.com>
2020-01-14 21:45:19 -0800

    company list working

--- src/companyList.js
+++ src/companyList.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import data from './data';
+const { companies } = data;
 
-const Company = () => <hr></hr>;
+const Company = () => {
+    return (
+        <div>
+            <h3>Companies</h3>
+            <div className="CompanyList">
+                <ul>
+                    {companies.map(company => {
+                        return <li key={company.id}>
+                            {company.name} {company.state}
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </div>
+    )
+}
 
 export default Company
